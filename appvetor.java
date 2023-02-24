a82d814cfdd47e8d84535846d5a9f2446ca815aa
paulonoff <paulinhohs70@gmail.com>
2023-02-23 21:57:28 -0300

    first commit

---
+++ appvetor.java
@@ -0,0 +1,14 @@
+package vetor;
+
+public class appvetor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			int [] arr = {10,-21,45,67,33,57,89,128,9,156};
+		
+			for (int i = 0; i < arr.length; i++) System.out.println("Element at index " + i + ": "+ arr[i]);
+		
+	}
+
+}
